80d38670aeb39075589482e5b621fe1c33658842
Graham Weldon <graham@grahamweldon.com>
2017-09-14 21:10:07 +0900

    Change to core origin for scaffolding

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=hello_string
 pkg_origin=predominant
 pkg_version="0.1.0"
-pkg_scaffolding=rakops/scaffolding-go
+pkg_scaffolding=core/scaffolding-go
 #pkg_source="http://github.com/predominant/hello_string"
 scaffolding_go_base_path=github.com/predominant
